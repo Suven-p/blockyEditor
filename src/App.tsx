16eccfa297050f9491c229f6d8e18da31163a5c6
Suven-p <pandeysuven@gmail.com>
2023-07-14 23:08:13 +0545

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import BlockyEditor from './components/BlockyEditor/BlocklyEditor';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <BlockyEditor />
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/BlockyEditor/BlocklyEditor.tsx
@@ -0,0 +1,128 @@
+import React, { useRef, useState } from 'react';
+
+import { BlocklyWorkspace, WorkspaceSvg, useBlocklyWorkspace } from 'react-blockly';
+import ConfigFiles from 'react-blockly/dist/initContent/content';
+import { ToolboxInfo } from 'blockly/core/utils/toolbox';
+import { javascriptGenerator } from 'blockly/javascript';
+import { pythonGenerator } from 'blockly/python';
+import { dartGenerator } from 'blockly/dart';
+
+
+
+const TestEditor = () => {
+  const onWorkspaceChange = (workspace: WorkspaceSvg) => {
+    workspace.registerButtonCallback('myFirstButtonPressed', () => {
+        alert('button is pressed');
+    });
+    const code = languageGenerator.workspaceToCode(workspace);
+    setGeneratedCode(code);
+  };
+
+    const [toolboxConfiguration, setToolboxConfiguration] =
+        React.useState<ToolboxInfo>(ConfigFiles.INITIAL_TOOLBOX_JSON);
+    const [generatedCode, setGeneratedCode] = useState('');
+    const [languageGenerator, setLanguageGenerator] =
+        useState(javascriptGenerator);
+    const blocklyRef = useRef(null);
+    const { workspace, xml } = useBlocklyWorkspace({
+      ref: blocklyRef,
+      toolboxConfiguration:toolboxConfiguration,
+      workspaceConfiguration:{
+          grid: {
+              spacing: 20,
+              length: 3,
+              colour: '#ccc',
+              snap: true,
+          },
+      },
+      initialXml:ConfigFiles.INITIAL_XML,
+      // initialJson:ConfigFiles.INITIAL_JSON,
+      // className:'fill-height',
+      onWorkspaceChange:onWorkspaceChange,
+    });
+
+    const languages = {
+        Javascript: javascriptGenerator,
+        Python: pythonGenerator,
+        Dart: dartGenerator,
+    };
+    const onLanguageChangeHandler = (event) => {
+        setLanguageGenerator(languages[event.target.value]);
+    };
+
+    React.useEffect(() => {
+        const code = languageGenerator.workspaceToCode(workspace);
+        setGeneratedCode(code);
+    }, [languageGenerator]);
+
+    React.useEffect(() => {
+        window.setTimeout(() => {
+            setToolboxConfiguration((prevConfig: ToolboxInfo) => ({
+                ...prevConfig,
+                contents: [
+                    ...prevConfig.contents,
+                    {
+                        kind: 'category',
+                        name: 'Dynamically added category',
+                        contents: [
+                            { kind: 'block', type: 'text' },
+                            {
+                                kind: 'block',
+                                blockxml:
+                                    '<block type="text_print"><value name="TEXT"><shadow type="text">abc</shadow></value></block>',
+                            },
+                        ],
+                    },
+                ],
+            }));
+        }, 2000);
+
+        window.setTimeout(() => {
+            setToolboxConfiguration((prevConfig: ToolboxInfo) => ({
+                ...prevConfig,
+                contents: [
+                    ...prevConfig.contents.slice(
+                        0,
+                        prevConfig.contents.length - 1
+                    ),
+                    {
+                        ...prevConfig.contents[prevConfig.contents.length - 1],
+                        contents: [{ kind: 'block', type: 'text' }],
+                    },
+                ],
+            }));
+        }, 4000);
+
+        window.setTimeout(() => {
+            setToolboxConfiguration((prevConfig: ToolboxInfo) => ({
+                ...prevConfig,
+                contents: [
+                    ...prevConfig.contents.slice(
+                        0,
+                        prevConfig.contents.length - 1
+                    ),
+                ],
+            }));
+        }, 10000);
+    }, []);
+
+    return (
+        <>
+            <label htmlFor='language'>Choose a language:</label>
+            <select onChange={onLanguageChangeHandler} id='language'>
+                {Object.keys(languages).map((language) => {
+                    return <option key={language}>{language}</option>;
+                })}
+            </select>
+
+            <div ref={blocklyRef} style={{width: '80%', height: '300px'}} />
+            <textarea
+                style={{ height: '200px', width: '400px' }}
+                value={generatedCode}
+                readOnly
+            />
+        </>
+    );
+};
+
+export default TestEditor;
